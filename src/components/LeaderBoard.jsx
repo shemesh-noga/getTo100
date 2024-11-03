1262cf1f9deee6126b3a21a885f7989d5f51b589
yakirahordes <yakirahordes@hilma.tech>
2024-11-03 14:32:46 +0200

    leader

--- src/components/LeaderBoard.jsx
+++ src/components/LeaderBoard.jsx
@@ -1,7 +1,15 @@
+import { allUsers, User, Yakira } from "../data/Users";
+
 export default function LeaderBoard({ currentPage }) {
+  const usersAverage = allUsers.map((user) => ({
+    username: user.name,
+    average: user.average,
+  }));
+  usersAverage.sort((a, b) => a.average - b.average);
+  console.log("usersAverage: ", usersAverage);
   return currentPage === "leaderBoard" ? (
     <>
-      <h1>leader</h1>
+      <h1>First Place: {}</h1>
     </>
   ) : null;
 }
--- src/data/Users.jsx
+++ src/data/Users.jsx
@@ -1,17 +1,21 @@
-export const allUsers = [
-  {
-    name: "Yakira",
-    scores: [5, 7, 8],
-  },
-  {
-    name: "Noga",
-    scores: [10, 5, 6],
-  },
-];
-
 export class User {
   constructor(name) {
     this.name = name;
     this.scores = [];
   }
+
+  get average() {
+    const score = this.scores;
+    const averageScore =
+      score.reduce((acc, val) => acc + val, 0) / score.length;
+    return averageScore;
+  }
 }
+
+export const Yakira = new User("Yakira");
+export const Noga = new User("Noga");
+
+Yakira.scores = [2, 3, 4];
+Noga.scores = [1, 2, 3];
+
+export const allUsers = [Yakira, Noga];
